414dedbcb025a12828096e90742b62d8c0be83c0
centinela1024tb <centinela1024tb@gmail.com>
2022-03-22 19:41:15 -0600

    [ADD] Se agregan las pages para el Auth
    Se agrega registrar | olvide-password | confirmar-cuenta

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+
+const Login = () => {
+  return (
+    <>      
+      <div>
+        <h1 className="text-indigo-600 font-blank text-6xl">Inicia Sesión y Administra tus <span className="text-black">Pacientes</span></h1>
+      </div>
+      <div>2</div>
+    </>
+  )
+}
+
+export default Login
---
+++ src/pages/OlvidePassword.jsx
@@ -0,0 +1,12 @@
+const OlvidePassword = () => {
+  return (
+    <>
+      
+        <div>1</div>
+        <div>2</div>
+      
+    </>
+  )
+}
+
+export default OlvidePassword
