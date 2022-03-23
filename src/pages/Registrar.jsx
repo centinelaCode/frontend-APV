77d2ca886dbd0616413fbf9bf18d2901fee1a097
centinela1024tb <centinela1024tb@gmail.com>
2022-03-23 14:58:14 -0600

    [ADD] Función del submit handleSubmit
    Validaciones: no existan campos vacios, que los password sean iguales y que sea de almenos 6 caracteres

--- src/pages/Registrar.jsx
+++ src/pages/Registrar.jsx
@@ -8,13 +8,40 @@ const Registrar = () => {
   const [password, setPassword] = useState('');
   const [repetirPassword, setRepetirPassword] = useState('');
 
+  // funciones
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // validamos que no existan campos vacios
+    if([nombre, email, password, repetirPassword].includes('')) {
+      console.log('Hay campos vacios');
+      return;
+    } 
+    
+    // validamos que los password sean iguales
+    if(password !== repetirPassword){
+      console.log('Los password no son iguales');
+      return
+    }
+
+    if(password.length < 6) {
+      console.log(password.length,'El password debe ser de al menos 6 caracteres');
+      console.log(repetirPassword.length)
+      return
+    }
+
+    console.log('despues if')
+  }
+
   return (    
     <>
       <div>
         <h1 className="text-indigo-600 font-blank text-6xl font-bold">Crea tu cuenta y Administra tus <span className="text-black font-bold">Pacientes</span></h1>
       </div>
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        <form>
+        <form
+          onSubmit={handleSubmit}
+        >
           <div className="my-5">
             <label
               className="uppercase text-gray-600 text-xl font-bold"
